be6fa9c3040ca706df7129cd4ce6dcfc53d93e50
Angela <angela.valdebenito.v@gmail.com>
2021-08-30 22:36:23 -0400

    Commit message

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,56 @@
 <template>
   <div>
     <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
-        <div class="container"><a class="navbar-brand">V8</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
-            <div
-                class="collapse navbar-collapse" id="navbarResponsive">
-                <!-- MENU DE NAVEGACION -->
-                <ul class="nav navbar-nav ml-auto" id="menu-v8">
-                    <li class="nav-item" role="presentation">
-                      <router-link to="/">Portada</router-link> 
-                    </li>
-                    <li class="nav-item" role="presentation">
-                      <router-link to="/SobreMi">Sobre mi</router-link>
-                    </li>
-                    <li class="nav-item" role="presentation">                    
-                      <router-link to="/Contacto">Contacto</router-link>
-                    </li>
-                    <li class="nav-item" role="presentation">                     
-                      <router-link to="/post/1">ultimo post</router-link> 
-                    </li>
-                </ul>
+      <div class="container">
+        <a class="navbar-brand">V8</a
+        ><button
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          class="navbar-toggler"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <!-- MENU DE NAVEGACION -->
+          <ul class="nav navbar-nav ml-auto" id="menu-v8">
+            <li class="nav-item" role="presentation">
+              <router-link to="/">Portada</router-link>
+            </li>
+            <li class="nav-item" role="presentation">
+              <router-link to="/SobreMi">Sobre mi</router-link>
+            </li>
+            <li class="nav-item" role="presentation">
+              <router-link to="/Contacto">Contacto</router-link>
+            </li>
+            <li class="nav-item" role="presentation">
+              <router-link to="/post/1">ultimo post</router-link>
+            </li>
+          </ul>
         </div>
-        </div>
+      </div>
     </nav>
     <!-- ROUTER VIEW QUE CARGARÁ LAS VISTAS (INICIO, SOBRE MÍ, CONTACTO, ETC.) -->
     <router-view />
   </div>
-  
 </template>
 
 <script>
-
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;  
+  margin-top: 60px;
 }
-#menu-v8 a{
+#menu-v8 a {
   text-shadow: 1px 1px #000;
 }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,47 +1,42 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Portada from './components/Portada'
-import Articulo from './components/Articulo' 
-import Contacto from './components/Contacto'
-import UltimoPost from './components/UltimoPost'
-import SobreMi from './components/SobreMi'
-import RutasInexistentes from './components/RutasInexistentes'
+import Vue from "vue";
+import Router from "vue-router";
+import Portada from "./components/Portada";
+import Articulo from "./components/Articulo";
+import Contacto from "./components/Contacto";
+import UltimoPost from "./components/UltimoPost";
+import SobreMi from "./components/SobreMi";
+import RutasInexistentes from "./components/RutasInexistentes";
 
-
-Vue.use(Router)
-export default new Router ({
-    mode: `history`,
-    routes:[{ 
-        path:'/Contacto', 
-        component: Contacto,
-         
+Vue.use(Router);
+export default new Router({
+  mode: `history`,
+  routes: [
+    {
+      path: "/Contacto",
+      component: Contacto,
     },
-    { 
-        path:'/', 
-        component: Portada,
-         
-    },  
-    { 
-        path:'/post/1', 
-        component: UltimoPost,
-        children:[{
-            path: 'Articulo',
-            component: Articulo, 
-        }], 
-        
+    {
+      path: "/",
+      component: Portada,
     },
-    { 
-        path:'/SobreMi', 
-        component: SobreMi,    
+    {
+      path: "/post/1",
+      component: UltimoPost,
+      children: [
+        {
+          path: "/post/1",
+          component: Articulo,
+          name: "Articulo",
+        },
+      ],
     },
-    { 
-        path:'/Articulo', 
-        component: Articulo,    
+    {
+      path: "/SobreMi",
+      component: SobreMi,
     },
-    { 
-        path:'*', 
-        component: RutasInexistentes 
+    {
+      path: "*",
+      component: RutasInexistentes,
     },
-]
-    
-})
+  ],
+});
